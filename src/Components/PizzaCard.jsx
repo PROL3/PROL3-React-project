39b39b8ad4b7d4aa9162751557e6f18b84bf56ce
liav <liavramadi17@gmail.com>
2024-08-04 22:59:55 +0300

    add button and son to menu

--- src/Components/PizzaCard.jsx
+++ src/Components/PizzaCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PizzaCard = ({ pizza }) => {
+const PizzaCard = ({ item }) => {
   const checkStrongBeer = (isStrong) => {
     return (
       <p className={`text-center ${isStrong ? "text-red-500" : "text-teal-500"}`}>
@@ -11,27 +11,31 @@ const PizzaCard = ({ pizza }) => {
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full h-48 object-cover" src={pizza.image} alt="Card image cap" />
+      <img className="w-full h-48 object-cover" src={item.image} alt="item image" />
       <div className="px-6 py-4">
         <p className="text-gray-700 text-base">
-          <span className="font-bold text-blue-500">Name:</span> {pizza.name}
+          <span className="font-bold text-blue-500">Name:</span> {item.name}
         </p>
         <p className="text-gray-700 text-base">
-          <span className="font-bold text-blue-500">Dish description:</span> {pizza.description}
+          <span className="font-bold text-blue-500">Description:</span> {item.description}
         </p>
+        {/* Check if the item type is item, then show beer pairing */}
+        {item.type === 'item' && item.beer_pairing && (
+          <p className="text-gray-700 text-base">
+            <span className="font-bold text-blue-500">Best pairing beer:</span> {item.beer_pairing}
+          </p>
+        )}
         <p className="text-gray-700 text-base">
-          <span className="font-bold text-blue-500">Best pairing beer:</span> {pizza.beer_pairing}
+          <span className="font-bold text-blue-500">Price:</span> {item.price}$
         </p>
-        <p className="text-gray-700 text-base">
-          <span className="font-bold text-blue-500">Price:</span> {pizza.price}$
-        </p>
-        {checkStrongBeer(pizza.strong_beer)}
+        {/* Check if the item has strong beer information */}
+        {item.isStrong !== undefined && checkStrongBeer(item.isStrong)}
         <button
           onClick={() => {
-            alert(pizza.name + " ");
+            alert(item.name + " ");
           }}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-        >
+          className=" mt-2 bg-gray-900 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
+          >
           More info
         </button>
       </div>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,60 +1,118 @@
-// components/Menu.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import { pizzaGenerator } from '../functions/pizzaGenerator';
-import { fetchImages } from '../functions/pixabayApi';
+import { filterArray } from '../functions/filterArray';
 import pizzaArray from '../PizzaArray.json';
+import Button from '../Components/Button';
+import { sliceArray } from '../functions/sliceArray';
+import NeBaButton from '../Components/NeBaButton';
 
 const Menu = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [choose, setChoose] = useState('');
-  const [images, setImages] = useState([]);
-  
-  // Filter pizzas based on the search term
-  const filteredPizzas = pizzaArray.filter(item =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const [currentArr, setCurrentArr] = useState(pizzaArray.slice(0, 3));
+  const [filteredArr, setFilteredArr] = useState(pizzaArray);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [inputPageNumber, setInputPageNumber] = useState('');
 
-  // Function to handle form submission
-  const send = async (e) => {
-    e.preventDefault();
-    if (choose === '') {
-      alert('Please choose an option');
-      return;
+  // Update filtered array whenever searchTerm or choose changes
+  useEffect(() => {
+    const newFilteredArr = filterArray(pizzaArray, choose, searchTerm);
+    setFilteredArr(newFilteredArr);
+    setCurrentArr(sliceArray(newFilteredArr, 1)); // Reset to the first page
+    setPageNumber(1); // Reset page number
+  }, [searchTerm, choose]);
+
+  const handleNextButtonClick = () => {
+    if (pageNumber < Math.ceil(filteredArr.length / 3)) {
+      setCurrentArr(sliceArray(filteredArr, pageNumber + 1));
+      setPageNumber(pageNumber + 1);
     }
-    await fetchImages(choose, setImages);
   };
 
-  
+  const handleBackButtonClick = () => {
+    if (pageNumber > 1) {
+      setCurrentArr(sliceArray(filteredArr, pageNumber - 1));
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  const handleButtonClick = (pageNumber) => {
+    setCurrentArr(sliceArray(filteredArr, pageNumber));
+    setPageNumber(pageNumber);
+  };
+
+  const buttonGenerator = (pages) => {
+    let buttons = [];
+    for (let i = 1; i <= pages; i++) {
+      buttons.push(<Button number={i} key={i} func={() => handleButtonClick(i)} page={pageNumber} />);
+    }
+    return buttons;
+  };
+
+  const goToPage = () => {
+    const page = parseInt(inputPageNumber, 10);
+    if (!isNaN(page) && page >= 1 && page <= Math.ceil(filteredArr.length / 3)) {
+      setCurrentArr(sliceArray(filteredArr, page));
+      setPageNumber(page);
+      setInputPageNumber('');
+    } else {
+      alert('Invalid page number');
+    }
+  };
+
   return (
     <div>
       <Header />
       <main className='container mx-auto mt-32'>
         <div className='flex justify-center items-center gap-3 mb-4'>
-          <form onSubmit={send} className='flex flex-col sm:flex-row items-center gap-2 mt-3'>
+          <form className='flex flex-col sm:flex-row items-center gap-2 mt-3'>
             <input
               type="text"
               onChange={(e) => setSearchTerm(e.target.value)}
               placeholder="Search for order..."
-              value={searchTerm} 
+              value={searchTerm}
               className='border rounded px-4 py-2'
             />
-            <select className='border-2 h-10' onChange={(e) => setChoose(e.target.value)}>
+            <select className='border-2 h-10' onChange={(e) => setChoose(e.target.value)} value={choose}>
               <option value="">Select a category</option>
               <option value="beer">Beer</option>
               <option value="pasta">Pasta</option>
               <option value="pizza">Pizza</option>
             </select>
-            <button
-              type="submit"
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-              Search
-            </button>
           </form>
         </div>
         <div className='mt-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center'>
-        {pizzaGenerator(images.length > 0 ? images : filteredPizzas)}
+          {currentArr.length > 0 ? pizzaGenerator(currentArr) : <p>No results found.  try another product.</p>}
+        </div>
+        <div className='flex justify-center items-center gap-3 mt-4'>
+          <NeBaButton
+            status="Back"
+            func={handleBackButtonClick}
+            disabled={pageNumber <= 1}
+          />
+          {buttonGenerator(Math.ceil(filteredArr.length / 3))}
+          <NeBaButton
+            status="Next"
+            func={handleNextButtonClick}
+            disabled={pageNumber >= Math.ceil(filteredArr.length / 3)}
+          />
+        </div>
+        <div className='flex justify-center items-center gap-3 mt-4 mb-4'>
+          <input
+            type="number"
+            value={inputPageNumber}
+            onChange={(e) => setInputPageNumber(e.target.value)}
+            placeholder="Go to page..."
+            className='border rounded px-4 py-2'
+          />
+          <button
+            onClick={goToPage}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          >
+            Go
+          </button>
         </div>
       </main>
       <Footer />
@@ -63,3 +121,4 @@ const Menu = () => {
 };
 
 export default Menu;
+  
